dc6498446101da55c2c7742ac434d476835d3b52
Jean-Philippe Cottin <jpcottin@bittorrent.com>
2015-01-18 22:06:52 -0800

    Initial commit
    Signed-off-by: Jean-Philippe Cottin <jpcottin@bittorrent.com>

---
+++ gallery/var/logs/2014-06-13.log.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2014-06-13 04:15:00 +00:00 --- error: File not found: login
+2014-06-13 04:15:43 +00:00 --- error: File not found: login
